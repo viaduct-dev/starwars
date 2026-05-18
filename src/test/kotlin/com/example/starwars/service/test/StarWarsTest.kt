c0d19e7c30e48cb8b71e3d2cf499ac948527e8fb
ViaBot <viabot@ductworks.io>
2026-05-18 13:39:56 +0000

    Update from airbnb/viaduct at main
    GitOrigin-RevId: e8bfd67e26afb86f1f50647d5eef47f318fc1988

--- src/test/kotlin/com/example/starwars/service/test/StarWarsTest.kt
+++ src/test/kotlin/com/example/starwars/service/test/StarWarsTest.kt
@@ -1,15 +1,13 @@
 package com.example.starwars.service.test
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
 
 /**
  * Basic test to verify that the Micronaut application context loads successfully.
  */
 @MicronautTest
-class StarWarsTest : StringSpec({
-    "should start application context" {
-        true shouldBe true
-    }
-})
+class StarWarsTest {
+    @Test
+    fun `should start application context`() = Unit
+}
